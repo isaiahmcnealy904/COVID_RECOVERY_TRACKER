10f609ab3618ee49dfe763480547dc43f2aeff95
Isaiah <isaiah_mcnealy_o@lilly.com>
2020-07-20 12:42:08 -0400

    Initial Commit

---
+++ COVID_RECOVERY_TRACKER/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVID_RECOVERY_TRACKER
+//
+//  Created by Isaiah McNealy on 7/20/20.
+//  Copyright © 2020 Isaiah McNealy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
